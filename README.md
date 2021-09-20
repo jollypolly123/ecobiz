2dfb951b9a3fb8208e56a97861ceaef23b334f34
jollypolly123 <rachelle.hu123@gmail.com>
2021-09-19 23:11:59 -0400

    backend

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# ecobiz
+# ECOBIZ
  
+To deploy changes, use:
+```
+gcloud builds submit --tag gcr.io/ecobiz-com/ecobiz
+gcloud beta run deploy ecobiz --image gcr.io/ecobiz-com/ecobiz
+
+firebase deploy
+```
+
+done pages:
+- resources
+- jobs
+- directory
+
+needs work:
+- login, register
+- postservices, postjoblisting
+- forgotpassword
--- server/src/app.py
+++ server/src/app.py
@@ -1,36 +1,80 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify, make_response
+from flask import Flask, render_template, request, redirect, session, url_for, jsonify, make_response
 from firebase_admin import credentials, firestore, initialize_app
+from functools import wraps
 import os
+import pyrebase
+import json
 import time
 
 app = Flask(__name__, static_folder='')
+app.config['SESSION_TYPE'] = 'memcached'
+app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
+
+config = {
+  "apiKey": "AIzaSyC3-64_FCq0MTfkz1yZxPgiMg6FjekjsyQ",
+  "authDomain": "ecobiz-com.firebaseapp.com",
+  "projectId": "ecobiz-com",
+  "storageBucket": "ecobiz-com.appspot.com",
+  "messagingSenderId": "530286252060",
+  "appId": "1:530286252060:web:febfb512972936c9d94fd9",
+  "measurementId": "G-KY5ZQDXGFB",
+  "databaseURL": "https://ecobiz-com-default-rtdb.firebaseio.com/"
+}
 
 cred = credentials.Certificate("key.json")
 default_app = initialize_app(cred)
-db = firestore.client()
-todo_ref = db.collection('todos')
+# db = firestore.client()
+pb = pyrebase.initialize_app(config)
+
+auth = pb.auth()
+db = pb.database()
 
 
-# @app.route('/', methods=['POST', 'GET'])
-# def index():
-#     if request.method == 'POST':
-#         return render_template('index.html')
-#     else:
-#         return render_template('index.html')
+def check_token(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        try:
+            user = auth.current_user
+        except:
+            return redirect('https://ecobiz-com.web.app/login')
+        return f(*args, **kwargs)
+    return wrap
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    user = ""
+    try:
+        user = session['usr']
+    except:
+        pass
+    return render_template('index.html', req=auth.current_user)
 
 
 @app.route('/directory')
 def directory():
-    return render_template('directory.html')
+    services = db.child("services").get().val()
+    service_list = []
+    service_row = []
+    for service in services:
+        service_row.append(service)
+        if len(service_row) == 3:
+            service_list.append(service_row)
+            service_row = []
+    return render_template('directory.html', service_list=service_list)
 
 
 @app.route('/jobs')
 def jobs():
-    return render_template('jobs.html')
+    job_listings = db.child("joblistings").get().val()
+    jobs_list = []
+    jobs_row = []
+    for service in job_listings:
+        jobs_row.append(service)
+        if len(jobs_row) == 3:
+            jobs_list.append(jobs_row)
+            jobs_row = []
+    return render_template('jobs.html', jobs_list=jobs_list)
 
 
 @app.route('/resources')
@@ -38,9 +82,30 @@ def resources():
     return render_template('resources.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        if email is None or password is None:
+            return render_template('login.html', error="Error missing email or password")
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            auth.refresh(user['refreshToken'])
+            return redirect('https://ecobiz-com.web.app/')
+        except Exception as e:
+            return render_template('login.html', error="Error logging in<br />{}".format(e))
     return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    auth.signOut()
+    try:
+        session.pop('usr', None)
+    except:
+        pass
+    return redirect('https://ecobiz-com.web.app/')
 
 
 @app.route('/forgotpassword')
@@ -48,18 +113,79 @@ def forgotpassword():
     return render_template('forgotpassword.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        if email is None or password is None:
+            return render_template('register.html', error="Error missing email or password")
+        elif len(password) < 6:
+            return render_template('register.html', error="Password must be greater than 6 characters", email=email)
+        try:
+            user = auth.create_user_with_email_and_password(email, password)
+            auth.refresh(user['refreshToken'])
+            return redirect('https://ecobiz-com.web.app/')
+        except Exception as e:
+            return render_template('register.html', error="Error creating user<br />{}".format(e))
     return render_template('register.html')
 
 
-@app.route('/postjoblisting')
+@app.route('/postjoblisting', methods=['POST', 'GET'])
+@check_token
 def postjoblisting():
+    if request.method == 'POST':
+        company = request.form.get('company', "No company anem")
+        jobTitle = request.form.get('jobTitle', "No job title")
+        skills_needed = request.form.get('skills-needed', "No skills needed")
+        description = request.form.get('description', "No description")
+        website = request.form.get('website', "No website")
+        linkedin = request.form.get('linkedin', "No LinkedIn")
+        instagram = request.form.get('instagram', "No Instagram")
+        application = request.form.get('application', "No application")
+        data = {
+            "application": application,
+            "company": company,
+            "jobTitle": jobTitle,
+            "description": description,
+            "website": website,
+            "linkedin": linkedin,
+            "instagram": instagram,
+            "skills_needed": skills_needed
+        }
+        try:
+            db.child("joblistings").push(data)
+            return redirect('https://ecobiz-com.web.app/directory')
+        except Exception as e:
+            return render_template('postjoblisting.html', error="Error creating listing<br />{}".format(e))
     return render_template('postjoblisting.html')
 
 
-@app.route('/postservices')
+@app.route('/postservices', methods=['POST', 'GET'])
+@check_token
 def postservices():
+    if request.method == 'POST':
+        full_name = request.form.get('full-name', "No name")
+        jobTitle = request.form.get('jobTitle', "No title")
+        skills = request.form.get('skills', "No skills")
+        bio = request.form.get('bio', "No bio")
+        website = request.form.get('website', "No website")
+        linkedin = request.form.get('linkedin', "No LinkedIn")
+        instagram = request.form.get('instagram', "No Instagram")
+        data = {
+            "full_name": full_name,
+            "jobTitle": jobTitle,
+            "skills": skills,
+            "website": website,
+            "linkedin": linkedin,
+            "instagram": instagram,
+            "bio": bio
+        }
+        try:
+            db.child("joblistings").push(data)
+            return redirect('https://ecobiz-com.web.app/directory')
+        except Exception as e:
+            return render_template('postjoblisting.html', error="Error creating listing<br />{}".format(e))
     return render_template('postservices.html')
 
 
